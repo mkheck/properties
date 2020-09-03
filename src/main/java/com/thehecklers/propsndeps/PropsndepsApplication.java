6d873fb62e1b79679ce362ca02371f39ba9d8293
Mark Heckler <mark.heckler@gmail.com>
2020-09-03 11:16:24 -0500

    Updated to current lib versions and added latest demo features

--- src/main/java/com/thehecklers/propsndeps/PropsndepsApplication.java
+++ src/main/java/com/thehecklers/propsndeps/PropsndepsApplication.java
@@ -2,11 +2,13 @@ package com.thehecklers.propsndeps;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.bind.Name;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,104 +23,58 @@ public class PropsndepsApplication {
 	}
 
 	@Bean
-	@ConfigurationProperties(prefix = "blackbox")
-	BlackBoxProperties bbProps() {
-		return new BlackBoxProperties();
+	@ConfigurationProperties(prefix = "supplier")
+	BeverageSupplier supplier() {
+		return new BeverageSupplier();
 	}
 }
 
-@RestController
-@RequestMapping("/bb")
 @AllArgsConstructor
-class BlackBoxController {
-	private final BlackBoxProperties bbProps;
-
-	@GetMapping
-	BlackBoxProperties getBbProps() {
-		return bbProps;
-	}
-}
-
 @RestController
-@RequestMapping("/dev")
-class DeveloperController {
-	@Value("${developer.first-name:Jenn}")
-	private String devName;
-
-	@Value("${developer.recognition}")
-	private String award;
-
-	@GetMapping("/name")
-	String getDevName() {
-		return devName;
-	}
-
-	@GetMapping("/award")
-	String getDevAward() {
-		return award;
-	}
-}
-
-@RestController
-@RequestMapping("/location")
-class LocationController {
-	private final LocationProperties locProps;
-
-	public LocationController(LocationProperties locProps) {
-		this.locProps = locProps;
-	}
-
-	@GetMapping
-	LocationProperties getLocProps() {
-		return locProps;
-	}
+@RequestMapping("/beverage")
+class BevController {
+	//	@Value("${beverage.name:Raktajino}")
+//	private String name;
+//	@Value("${beverage.brew-method:Bespoke ${beverage.name:Earl Grey, hot}}")
+//	private String brewMethod;
+	private final HotBeverage beverage;
 
 	@GetMapping("/name")
 	String getName() {
-		return locProps.getName();
+		return beverage.getName();
 	}
 
-	@GetMapping("/description")
-	String getDescription() {
-		return locProps.getDescription();
-	}
-
-	@GetMapping("/region")
-	String getRegion() {
-		return locProps.getRegion();
+	@GetMapping("/brewmethod")
+	String getBrewMethod() {
+		return beverage.getBrewMethod();
 	}
 }
 
-@ConfigurationProperties(prefix = "location")
-class LocationProperties {
-	private String name, description, region;
+@AllArgsConstructor
+@RestController
+@RequestMapping("/supplier")
+class SupController {
+	private final BeverageSupplier supplier;
 
-	public String getName() {
-		return name;
+	@GetMapping
+	BeverageSupplier getSupplier() {
+		return supplier;
 	}
+}
 
-	public void setName(String name) {
+@Value
+@ConfigurationProperties(prefix = "beverage")
+class HotBeverage {
+	String name, brewMethod;
+
+	@ConstructorBinding
+	public HotBeverage(String name, @Name("brew") String brewMethod) {
 		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getRegion() {
-		return region;
-	}
-
-	public void setRegion(String region) {
-		this.region = region;
+		this.brewMethod = brewMethod;
 	}
 }
 
 @Data
-class BlackBoxProperties {
-	private String propertyId, propertyDescription;
+class BeverageSupplier {
+	String name, location;
 }
